6a7f967d85d2a0c522dbcc9eb6590b4b69f12fa0
Yuji Toshihiro <85441887+honganji@users.noreply.github.com>
2023-11-02 16:49:35 +0900

    Revert "enable to connect to remote db"

--- src/main/java/com/example/animecollectionapiv2/AnimeCollectionApiV2Application.java
+++ src/main/java/com/example/animecollectionapiv2/AnimeCollectionApiV2Application.java
@@ -1,6 +1,5 @@
 package com.example.animecollectionapiv2;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,13 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AnimeCollectionApiV2Application {
 
 	public static void main(String[] args) {
-//		Dotenv dotenv = Dotenv.configure().load();
-//
-//		// Access the values in your application.properties
-//		System.setProperty("spring.datasource.url", dotenv.get("AIVEN_DB_URL"));
-//		System.setProperty("spring.datasource.username", dotenv.get("AIVEN_USER_NAME"));
-//		System.setProperty("spring.datasource.password", dotenv.get("AIVEN_PASSWORD"));
-
 		SpringApplication.run(AnimeCollectionApiV2Application.class, args);
 	}
 
